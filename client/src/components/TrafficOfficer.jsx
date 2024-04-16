adcd5cc94ccdbd82b2bbb2dae176c5b24bceb0af
barnik109 <maitybarnik@gmail.com>
2024-04-16 21:32:21 +0530

    Resolved refresh feature

--- client/src/components/TrafficOfficer.jsx
+++ client/src/components/TrafficOfficer.jsx
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 import police from '../assets/police.gif'
 
 const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
-  
+
   const navigate = useNavigate();
 
   const [enteringLicenseNumber, setEnteringLicenseNumber] = useState("");
@@ -14,14 +14,25 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
   const [checkingLicenseNumber, setCheckingLicenseNumber] = useState("");
   const [matchedViolations, setMatchedViolations] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [badgeNumber, setBadgeNumber] = useState("");
 
-  // console.log(officerBadgeNumber)
   useEffect(() => {
     const loggedIn = localStorage.getItem("isLoggedIn") === "true";
     setIsLoggedIn(loggedIn);
+
+    // Retrieve badge number from local storage
+    const storedBadgeNumber = localStorage.getItem("officerBadgeNumber");
+    if (storedBadgeNumber) {
+      setBadgeNumber(storedBadgeNumber);
+    } else if (officerBadgeNumber) {
+      setBadgeNumber(officerBadgeNumber);
+    }
   }, []);
 
-
+  // Function to save badge number to local storage
+  const saveBadgeNumberToLocalStorage = (badgeNumber) => {
+    localStorage.setItem("officerBadgeNumber", badgeNumber);
+  };
 
   const handleEnteringLicenseChange = (e) => {
     setEnteringLicenseNumber(e.target.value);
@@ -40,13 +51,12 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
   };
 
   const handleLogout = () => {
-    // Clear login state and redirect to login page
     localStorage.setItem("isLoggedIn", "false");
     setIsLoggedIn(false);
+    localStorage.removeItem("officerBadgeNumber");
     alert("You have been logged out successfully");
     navigate("/");
   };
-
 
   const submitViolation = async (event) => {
     event.preventDefault();
@@ -54,9 +64,7 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
       const offenceType = convertViolationTypeToCode(violationType);
       const tx = await contract.processViolation(offenceType);
       await tx.wait();
-      
 
-      // Send violation details to the backend API
       const response = await fetch('http://localhost:3001/submit-violation', {
         method: 'POST',
         headers: {
@@ -72,29 +80,23 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
 
       const result = await response.json();
 
-      // Check if the violation details were submitted successfully
       if (result.success) {
-        // Update frontend state or show a success message
         console.log(result.message);
       } else {
-        // Handle error or show an error message
         console.error(result.message);
       }
     } catch (error) {
-      // Handle error or show a notification
       console.error(error);
     }
   };
 
   const checkViolationsDetails = () => {
-    // Filter stored violations based on entered license number
     const matched = storedViolations.filter(
       (violation) => violation.drivingLicenseNumber === checkingLicenseNumber
     );
     setMatchedViolations(matched);
   };
 
-  // Helper function to convert violation type to code
   const convertViolationTypeToCode = (violationType) => {
     const violationTypeCodes = {
       "GeneralOffence": 1,
@@ -124,10 +126,7 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
     return violationTypeCodes[violationType];
   };
 
-  // Helper function to calculate demerit points based on violation type
   const calculateDemeritPoints = (offenceType) => {
-    // Implement your logic to calculate demerit points based on offenceType
-    // For simplicity, using a basic mapping in this example
     const demeritPointsMapping = {
       1: 5,
       2: 10,
@@ -152,77 +151,27 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
       21: 20,
       22: 20,
       23: 10,
-      // Add other mappings as needed
     };
     return demeritPointsMapping[offenceType] || 0;
   };
 
-  return (
-    // <div className="bg-white h-screen flex items-center justify-center">
-    //   <div className="bg-gray-200 rounded-3xl shadow-lg w-[30rem] h-[] flex flex-col items-center py-5">
-    //     <h2 className="text-2xl">Traffic Officer Panel</h2>
-    //     <div className="p-6 flex flex-col">
-    //       <div>
-    //         <label className="font-bold block">Entering License Number:</label>
-    //         <input
-    //           type="text"
-    //           value={enteringLicenseNumber}
-    //           onChange={handleEnteringLicenseChange}
-    //           className="rounded border border-solid border-slate-100 shadow-lg px-5 py-2 mb-5 w-full block"
-    //         />
-    //       </div>
-    //       <div>
-    //         <label className="font-bold block">Violation Date:</label>
-    //         <input
-    //           type="date"
-    //           value={violationDate}
-    //           onChange={handleViolationDateChange}
-    //           max={new Date().toISOString().split('T')[0]}
-    //           className="rounded border border-solid border-slate-100 shadow-lg px-5 py-2 mb-5 w-full block"
-    //         />
-    //       </div>
-    //       <div>
-    //         <label className="font-bold block">Violation Type:</label>
-    //         <select value={violationType} onChange={handleViolationTypeChange} className="rounded border border-solid border-slate-100 shadow-lg px-5 py-2 mb-5 w-full block">
-    //           <option value="GeneralOffence">General Offence (e.g., improper number plate, illegal parking, etc.)</option>
-    //           <option value="NotObeyingAuthorities">Not obeying the orders from the Authorities/Not sharing information</option>
-    //           <option value="WithoutValidLicense">Driving/riding without a valid Driving Licence</option>
-    //           <option value="AfterDisqualification">Driving/riding a vehicle after disqualification</option>
-    //           <option value="Overspeeding">Overspeeding</option>
-    //           <option value="RashDriving">Rash/dangerous driving</option>
-    //           <option value="UnderInfluence">Driving/riding under the influence of intoxicating substances/alcohol</option>
-    //           <option value="UnfitState">Driving/riding in a mentally/physically unfit state</option>
-    //           <option value="WithoutInsurance">Driving/riding a vehicle without valid motor insurance</option>
-    //           <option value="IllegalRacing">Illegal racing and overspeeding</option>
-    //           <option value="OversizedVehicle">Driving an oversized vehicle</option>
-    //           <option value="AccidentRelatedOffences">Accident-related offences</option>
-    //           <option value="WithoutRegistration">Driving/riding a vehicle without a valid Registration Certificate</option>
-    //           <option value="WithoutPermit">Driving a vehicle without permit</option>
-    //           <option value="UsingMobileWhileDriving">Driving/riding a vehicle while using a mobile phone</option>
-    //           <option value="OverloadingVehicle">Overloading the vehicle</option>
-    //           <option value="OverloadingPassengers">Overloading passengers</option>
-    //           <option value="OverloadingTwoWheeler">Overloading a two-wheeler</option>
-    //           <option value="NoHelmet">Not wearing a helmet while riding a two-wheeler</option>
-    //           <option value="NoSeatbelt">Not wearing seatbelt while driving</option>
-    //           <option value="NotGivingWay">Not giving way for emergency vehicles</option>
-    //           <option value="JuvenileOffences">Offences committed by juveniles</option>
-    //           <option value="UsageOfHorn">Usage of horn in Silent Zone</option>
-            
+  // Call the function to save badge number to local storage when it changes
+  useEffect(() => {
+    if (badgeNumber) {
+      saveBadgeNumberToLocalStorage(badgeNumber);
+    }
+  }, [badgeNumber]);
 
-             
-    //           {/* Add other violation types as needed */}
-    //         </select>
-    //       </div>
-    //       <button onClick={submitViolation} className="bg-blue-700 text-white rounded-lg mx-10 px-5 py-2 hover:bg-blue-500">Submit</button>
-    //     </div>
-    //   </div>
-    //   <img src={police  } alt="" />
-    // </div>
+  return (
     <div>
-      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center tailwind-scrollbar-hide ">
+      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center tailwind-scrollbar-hide">
         <div className="flex items-center">
-          <span className="mr-4">Welcome, {officerBadgeNumber}</span>
-          <button onClick={handleLogout} className="bg-red-600 hover:bg-red-700 px-3 py-1 rounded-md">Logout</button>
+          <span className="mr-4">Welcome, {badgeNumber}</span>
+        </div>
+        <div className="flex items-center">
+          <button onClick={handleLogout} className="bg-red-600 hover:bg-red-700 px-3 py-1 rounded-md mr-4">
+            Logout
+          </button>
         </div>
       </nav>
       <div className=" h-screen flex items-center justify-center tailwind-scrollbar-hide">
@@ -251,30 +200,29 @@ const TrafficOfficer = ({ contract, officerBadgeNumber }) => {
             <div>
               <label className="font-bold block">Violation Type:</label>
               <select value={violationType} onChange={handleViolationTypeChange} className="rounded border border-solid border-slate-100 shadow-lg px-5 py-2 mb-5 w-full block">
-                           <option value="GeneralOffence">General Offence (e.g., improper number plate, illegal parking, etc.)</option>
-                           <option value="NotObeyingAuthorities">Not obeying the orders from the Authorities/Not sharing information</option>
-                           <option value="WithoutValidLicense">Driving/riding without a valid Driving Licence</option>
-                           <option value="AfterDisqualification">Driving/riding a vehicle after disqualification</option>
-                           <option value="Overspeeding">Overspeeding</option>
-                           <option value="RashDriving">Rash/dangerous driving</option>
-                           <option value="UnderInfluence">Driving/riding under the influence of intoxicating substances/alcohol</option>
-                           <option value="UnfitState">Driving/riding in a mentally/physically unfit state</option>
-                           <option value="WithoutInsurance">Driving/riding a vehicle without valid motor insurance</option>
-                           <option value="IllegalRacing">Illegal racing and overspeeding</option>
-                           <option value="OversizedVehicle">Driving an oversized vehicle</option>
-                           <option value="AccidentRelatedOffences">Accident-related offences</option>
-                           <option value="WithoutRegistration">Driving/riding a vehicle without a valid Registration Certificate</option>
-                           <option value="WithoutPermit">Driving a vehicle without permit</option>
-                           <option value="UsingMobileWhileDriving">Driving/riding a vehicle while using a mobile phone</option>
-                           <option value="OverloadingVehicle">Overloading the vehicle</option>
-                           <option value="OverloadingPassengers">Overloading passengers</option>
-                           <option value="OverloadingTwoWheeler">Overloading a two-wheeler</option>
-                           <option value="NoHelmet">Not wearing a helmet while riding a two-wheeler</option>
-                           <option value="NoSeatbelt">Not wearing seatbelt while driving</option>
-                           <option value="NotGivingWay">Not giving way for emergency vehicles</option>
-                           <option value="JuvenileOffences">Offences committed by juveniles</option>
-                           <option value="UsageOfHorn">Usage of horn in Silent Zone</option>
-                {/* Add other violation types as options */}
+                <option value="GeneralOffence">General Offence (e.g., improper number plate, illegal parking, etc.)</option>
+                <option value="NotObeyingAuthorities">Not obeying the orders from the Authorities/Not sharing information</option>
+                <option value="WithoutValidLicense">Driving/riding without a valid Driving Licence</option>
+                <option value="AfterDisqualification">Driving/riding a vehicle after disqualification</option>
+                <option value="Overspeeding">Overspeeding</option>
+                <option value="RashDriving">Rash/dangerous driving</option>
+                <option value="UnderInfluence">Driving/riding under the influence of intoxicating substances/alcohol</option>
+                <option value="UnfitState">Driving/riding in a mentally/physically unfit state</option>
+                <option value="WithoutInsurance">Driving/riding a vehicle without valid motor insurance</option>
+                <option value="IllegalRacing">Illegal racing and overspeeding</option>
+                <option value="OversizedVehicle">Driving an oversized vehicle</option>
+                <option value="AccidentRelatedOffences">Accident-related offences</option>
+                <option value="WithoutRegistration">Driving/riding a vehicle without a valid Registration Certificate</option>
+                <option value="WithoutPermit">Driving a vehicle without permit</option>
+                <option value="UsingMobileWhileDriving">Driving/riding a vehicle while using a mobile phone</option>
+                <option value="OverloadingVehicle">Overloading the vehicle</option>
+                <option value="OverloadingPassengers">Overloading passengers</option>
+                <option value="OverloadingTwoWheeler">Overloading a two-wheeler</option>
+                <option value="NoHelmet">Not wearing a helmet while riding a two-wheeler</option>
+                <option value="NoSeatbelt">Not wearing seatbelt while driving</option>
+                <option value="NotGivingWay">Not giving way for emergency vehicles</option>
+                <option value="JuvenileOffences">Offences committed by juveniles</option>
+                <option value="UsageOfHorn">Usage of horn in Silent Zone</option>
               </select>
             </div>
             <button onClick={submitViolation} className="bg-blue-700 text-white rounded-lg mx-10 px-5 py-2 hover:bg-blue-500">Submit</button>
