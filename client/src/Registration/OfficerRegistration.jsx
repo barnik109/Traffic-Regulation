8c2d5e71cb02fcb118a78c117e5c3e38de6c68d4
maihunsandip <skole472@gmail.com>
2024-04-17 21:16:34 +0530

    Restyled Registration page

--- client/src/Registration/OfficerRegistration.jsx
+++ client/src/Registration/OfficerRegistration.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { motion } from "framer-motion";
 
 const OfficerRegistration = ({ handleRegistrationSuccess }) => {
     const [formData, setFormData] = useState({
@@ -34,35 +35,42 @@ const OfficerRegistration = ({ handleRegistrationSuccess }) => {
     };
 
     return (
-        <div className="content bg-content-background overflow-hidden">
-            <div className="backGround overflow-y-auto py-6 flex flex-col h-screen sm:py-12">
-                <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-                    <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-                    <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                        <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
-                            <h2 className="text-4xl font-semibold mb-10 text-black">Officer Registration</h2>
+        <>
+            <section className='bg-gray-900'>
+                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <div class="bg-gray-800 w-1/2 rounded-xl">
+                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Register your Account
+                            </h1>
                             {error && <p className="text-red-500 mb-4">{error}</p>}
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-4">
-                                    <label htmlFor="badgeNumber" className="block text-lg font-medium text-gray-700">Badge Number:</label>
-                                    <input type="text" id="badgeNumber" name="badgeNumber" value={formData.badgeNumber} onChange={handleChange} className="mt-1 p-2 w-full border border-gray-300 rounded-md text-black" />
+                            <form onSubmit={handleSubmit} class="space-y-4 md:space-y-6">
+                                <div className=" flex flex-col justify-center items-center gap-10 mb-10">
+                                    <div className='w-1/2'>
+                                        <label for="badgeNumber" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bedge Number</label>
+                                        <input type="text" id="badgeNumber" name="badgeNumber" value={formData.badgeNumber} onChange={handleChange} class=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your username" required="" />
+                                    </div>
+                                    <div className='w-1/2'>
+                                        <label for="department" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
+                                        <input type="text" id="department" name="department" value={formData.department} onChange={handleChange} class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                    </div>
+                                    <div className='w-1/2'>
+                                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    </div>
                                 </div>
-                                <div className="mb-4">
-                                    <label htmlFor="department" className="block text-lg font-medium text-gray-700">Department:</label>
-                                    <input type="text" id="department" name="department" value={formData.department} onChange={handleChange} className="mt-1 p-2 w-full border border-gray-300 rounded-md text-black" />
+                                <div className="flex flex-wrap gap-10 justify-center items-center">
+                                    <motion.button whileHover={{ scale: 1.2 }} type="submit" class="w-[200px] text-white bg-gray-600 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</motion.button>
                                 </div>
-                                <div className="mb-4">
-                                    <label htmlFor="password" className="block text-lg font-medium text-gray-700">Password:</label>
-                                    <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} className="mt-1 p-2 w-full border border-gray-300 rounded-md text-black" />
-                                </div>
-                                <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">Register</button>
+                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Alredy have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</a>
+                                </p>
                             </form>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-
+            </section>
+        </>
 
     );
 };
